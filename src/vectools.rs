ee480375b25b39fab031e45f383334451daeb995
UseK <use.k.james@gmail.com>
2020-06-07 08:44:26 +0900

    Make Vectools public, use Dock-tests

--- src/vectools.rs
+++ src/vectools.rs
@@ -1,23 +1,17 @@
-trait Vectools {
+pub trait Vectools {
     fn keep(&mut self, bools: &Vec<bool>);
 }
 
 impl<T> Vectools for Vec<T> {
+    /// ```
+    /// use competitive_tools_rust::vectools::Vectools;
+    /// let mut v = vec![1, 2, 3, 4, 5];
+    /// let bools = vec![false, true, true, false, true];
+    /// v.keep(&bools);
+    /// assert_eq!(v, vec![2, 3, 5]);
+    /// ```
     fn keep(&mut self, bools: &Vec<bool>) {
         let mut i = 0;
         self.retain(|_| (bools[i], i +=1).0)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::vectools::Vectools;
-
-    #[test]
-    fn test() {
-        let mut v = vec![1, 2, 3, 4, 5];
-        let bools = vec![false, true, true, false, true];
-        v.keep(&bools);
-        assert_eq!(v, vec![2, 3, 5]);
     }
 }
