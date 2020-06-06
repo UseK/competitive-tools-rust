3d81d1af5afbbeacb6d2f291f520c55fb58a4100
UseK <use.k.james@gmail.com>
2020-06-06 10:36:41 +0900

    Delete min_prime_factor(), implement smallest_prime_factor()

--- src/math.rs
+++ src/math.rs
@@ -14,8 +14,10 @@
 /// assert_eq!(is_prime(997764507000), false);
 /// ```
 pub fn is_prime(n: usize) -> bool {
-    if n < 2 { return false }
-    min_prime_factor(n).is_none()
+    match smallest_prime_factor(n) {
+        Some(factor) => factor == n,
+        None => false,
+    }
 }
 
 /// ```
@@ -29,36 +31,34 @@ pub fn is_prime(n: usize) -> bool {
 /// assert_eq!(prime_factors(997764507000), vec![2, 2, 2, 3, 3, 3, 5, 5, 5, 6079, 6079]);
 /// ```
 pub fn prime_factors(mut n: usize) -> Vec<usize> {
-    if n < 2 { return vec![] }
     let mut factors = vec![];
-    while let Some(i) = min_prime_factor(n) {
+    while let Some(i) = smallest_prime_factor(n) {
         factors.push(i);
         n = n / i;
     }
-    factors.push(n);
     factors
 }
 
 /// ```
-/// use competitive_tools_rust::math::min_prime_factor;
-/// assert_eq!(min_prime_factor(0), None);
-/// assert_eq!(min_prime_factor(1), None);
-/// assert_eq!(min_prime_factor(2), None);
-/// assert_eq!(min_prime_factor(3), None);
-/// assert_eq!(min_prime_factor(4), Some(2));
-/// assert_eq!(min_prime_factor(5), None);
-/// assert_eq!(min_prime_factor(6), Some(2));
-/// assert_eq!(min_prime_factor(7), None);
-/// assert_eq!(min_prime_factor(8), Some(2));
-/// assert_eq!(min_prime_factor(9), Some(3));
-/// assert_eq!(min_prime_factor(1000000007), None);
-/// assert_eq!(min_prime_factor(997764507000), Some(2));
+/// use competitive_tools_rust::math::smallest_prime_factor;
+/// assert_eq!(smallest_prime_factor(0), None);
+/// assert_eq!(smallest_prime_factor(1), None);
+/// assert_eq!(smallest_prime_factor(2), Some(2));
+/// assert_eq!(smallest_prime_factor(3), Some(3));
+/// assert_eq!(smallest_prime_factor(4), Some(2));
+/// assert_eq!(smallest_prime_factor(5), Some(5));
+/// assert_eq!(smallest_prime_factor(6), Some(2));
+/// assert_eq!(smallest_prime_factor(7), Some(7));
+/// assert_eq!(smallest_prime_factor(8), Some(2));
+/// assert_eq!(smallest_prime_factor(9), Some(3));
+/// assert_eq!(smallest_prime_factor(1000000007), Some(1000000007));
+/// assert_eq!(smallest_prime_factor(997764507000), Some(2));
 /// ```
-pub fn min_prime_factor(n: usize) -> Option<usize> {
-    if n < 4 { return None }
+pub fn smallest_prime_factor(n: usize) -> Option<usize> {
+    if n < 2 { return None }
     let max_sqrt = (n as f64).sqrt().ceil() as usize;
     for i in 2..=max_sqrt {
         if n % i == 0 { return Some(i) }
     }
-    None
+    Some(n)
 }
