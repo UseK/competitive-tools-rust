9e60ae0dd2df76bf5e4da37991da26a1db07361d
UseK <use.k.james@gmail.com>
2020-06-07 08:55:05 +0900

    Implement count_items() as function, #TODO I want to do as Method

--- src/vectools.rs
+++ src/vectools.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub trait Vectools {
     fn keep(&mut self, bools: &Vec<bool>);
 }
@@ -14,4 +16,23 @@ impl<T> Vectools for Vec<T> {
         let mut i = 0;
         self.retain(|_| (bools[i], i +=1).0)
     }
+}
+
+/// ```
+/// use competitive_tools_rust::vectools::count_items;
+/// use std::collections::HashMap;
+/// let v = [2, 5, 8, 5];
+/// let mut expected = HashMap::new();
+/// expected.insert(2, 1);
+/// expected.insert(5, 2);
+/// expected.insert(8, 1);
+/// assert_eq!(count_items(&v), expected);
+/// ```
+pub fn count_items<T>(v: &[T]) -> HashMap<T, usize>
+    where T: std::cmp::Eq + std::hash::Hash + Copy
+{
+    v.iter().fold(HashMap::new(),|mut counter, &item| {
+        *counter.entry(item).or_insert(0) += 1;
+        counter
+    })
 }
