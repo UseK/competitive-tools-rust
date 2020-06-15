3e1492293ee8e068e9e73a151a89941e7d68ab1c
UseK <use.k.james@gmail.com>
2020-06-15 23:52:39 +0900

    Delete warning, PermutationWithReplacement is WIP

--- src/my_itertools/permutation_with_replacement.rs
+++ src/my_itertools/permutation_with_replacement.rs
@@ -1,21 +1,23 @@
 use std::ops::Range;
 
-pub struct PermutationWithReplacement<I>
+#[allow(dead_code)]
+struct PermutationWithReplacement<I>
 where
     I: Iterator,
     I::Item: Clone,
 {
-    k: usize,
-    indices_source: Range<usize>,
-    pool: Vec<I::Item>,
+    _k: usize,
+    _indices_source: Range<usize>,
+    _pool: Vec<I::Item>,
 }
 
 /// ```
-/// use competitive_tools_rust::my_itertools::permutation_with_replacement::permutation_with_replacement;
-/// let mut perm = permutation_with_replacement(0..3, 2);
-/// assert_eq!(perm.next(), Some(vec![0, 0, 0]))
+/// //use competitive_tools_rust::my_itertools::permutation_with_replacement::permutation_with_replacement;
+/// //let mut perm = permutation_with_replacement(0..3, 2);
+/// //assert_eq!(perm.next(), Some(vec![0, 0, 0]))
 /// ```
-pub fn permutation_with_replacement<I>(iter: I, k: usize) -> PermutationWithReplacement<I>
+#[allow(dead_code)]
+fn permutation_with_replacement<I>(iter: I, k: usize) -> PermutationWithReplacement<I>
 where
     I: Iterator,
     I::Item: Clone,
@@ -23,13 +25,14 @@ where
     let pool: Vec<I::Item> = iter.collect();
     let indices_source = 0..k.pow(pool.len() as u32);
     PermutationWithReplacement {
-        k,
-        indices_source,
-        pool,
+        _k: k,
+        _indices_source: indices_source,
+        _pool: pool,
     }
 }
 
 
+#[allow(dead_code)]
 impl<I> Iterator for PermutationWithReplacement<I>
 where
     I: Iterator,
@@ -38,8 +41,8 @@ where
     type Item = Vec<I::Item>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let index_source = self.indices_source.next()?;
-        let mut next_vec = Vec::new();
+        let _index_source = self._indices_source.next()?;
+        let next_vec = Vec::new();
         Some(next_vec)
     }
 }
