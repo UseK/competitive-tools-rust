6951c7fcefbb53c57021751911c1903c0d34a3ed
UseK <use.k.james@gmail.com>
2021-01-01 10:03:32 +0900

    Implement binary search search::[T]::lower_bound, upper_bound

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,6 @@ pub mod io;
 pub mod math;
 pub mod maze;
 pub mod my_itertools;
+pub mod search;
 pub mod union_find;
 pub mod vectools;
---
+++ src/search.rs
@@ -0,0 +1,68 @@
+use std::cmp::Ordering;
+
+pub trait BinarySearch<T> {
+    fn lower_bound(&self, x: &T) -> usize;
+    fn upper_bound(&self, x: &T) -> usize;
+}
+
+impl<T: Ord> BinarySearch<T> for [T] {
+    ///
+    /// ```
+    /// use competitive_tools_rust::search::BinarySearch;
+    /// let v = vec![2, 3, 3, 5, 6];
+    /// assert_eq!(v.lower_bound(&0), 0);
+    /// assert_eq!(v.lower_bound(&1), 0);
+    /// assert_eq!(v.lower_bound(&2), 0);
+    /// assert_eq!(v.lower_bound(&3), 1);
+    /// assert_eq!(v.lower_bound(&4), 3);
+    /// assert_eq!(v.lower_bound(&5), 3);
+    /// assert_eq!(v.lower_bound(&6), 4);
+    /// assert_eq!(v.lower_bound(&7), 5);
+    /// assert_eq!(v.lower_bound(&8), 5);
+    /// ```
+    ///
+    fn lower_bound(&self, x: &T) -> usize {
+        fn rec<T: Ord>(me: &[T], x: &T, left: usize, right: usize) -> usize {
+            if left == right {
+                left
+            } else {
+                let mid = (left + right) / 2;
+                match me[mid].cmp(x) {
+                    Ordering::Less => rec(me, x, mid + 1, right),
+                    Ordering::Equal | Ordering::Greater => rec(me, x, left, mid),
+                }
+            }
+        }
+        rec(self, x, 0, self.len())
+    }
+
+    ///
+    /// ```
+    /// use competitive_tools_rust::search::BinarySearch;
+    /// let v = vec![2, 3, 3, 5, 6];
+    /// assert_eq!(v.upper_bound(&0), 0);
+    /// assert_eq!(v.upper_bound(&1), 0);
+    /// assert_eq!(v.upper_bound(&2), 1);
+    /// assert_eq!(v.upper_bound(&3), 3);
+    /// assert_eq!(v.upper_bound(&4), 3);
+    /// assert_eq!(v.upper_bound(&5), 4);
+    /// assert_eq!(v.upper_bound(&6), 5);
+    /// assert_eq!(v.upper_bound(&7), 5);
+    /// assert_eq!(v.upper_bound(&8), 5);
+    /// ```
+    ///
+    fn upper_bound(&self, x: &T) -> usize {
+        fn rec<T: Ord>(me: &[T], x: &T, left: usize, right: usize) -> usize {
+            if left == right {
+                left
+            } else {
+                let mid = (left + right) / 2;
+                match me[mid].cmp(x) {
+                    Ordering::Less | Ordering::Equal => rec(me, x, mid + 1, right),
+                    Ordering::Greater => rec(me, x, left, mid),
+                }
+            }
+        }
+        rec(self, x, 0, self.len())
+    }
+}
