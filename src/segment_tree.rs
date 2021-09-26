618194facf7f60a3e6bbe11087064b20fd16e871
UseK <use.k.james@gmail.com>
2021-09-26 13:39:29 +0900

    Snapshot: Test SegmentTree::update

--- src/segment_tree.rs
+++ src/segment_tree.rs
@@ -1,7 +1,7 @@
 pub struct SegmentTree {
     pub n: usize,
     // Complete Binary Tree
-    tree: Vec<usize>,
+    pub tree: Vec<usize>,
 }
 
 /// RMQ (Range Minimum Query)
@@ -26,6 +26,27 @@ impl SegmentTree {
         }
     }
 
+    ///    1,
+    ///   3,          1,
+    ///  3,    7,    4,    1,
+    /// 5, 3, 7, 9, 6, 4, 1, 2]
+    /// ```
+    /// use competitive_tools_rust::segment_tree::SegmentTree;
+    /// let mut seg = SegmentTree::new(8);
+    /// seg.update(0, 5);
+    /// seg.update(1, 3);
+    /// seg.update(2, 7);
+    /// seg.update(3, 9);
+    /// seg.update(4, 6);
+    /// seg.update(5, 4);
+    /// seg.update(6, 1);
+    /// seg.update(7, 2);
+    /// assert_eq!(seg.tree, vec![
+    ///  1,
+    ///  3, 1,
+    ///  3, 7, 4, 1,
+    ///  5, 3, 7, 9, 6, 4, 1, 2]);
+    /// ```
     pub fn update(&mut self, ind: usize, value: usize) {
         let mut actual_ind = ind + self.n - 1;
         self.tree[actual_ind] = value;
